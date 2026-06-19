1f720ce3cad63e205f49373462603c95a7322718
Shahryar Pi <dev.shahryar@gmail.com>
2026-06-19 23:15:41 +0330

    refactor: change main menu in lg and md screen

--- app/components/navSide/navSide.tsx
+++ app/components/navSide/navSide.tsx
@@ -1,10 +1,18 @@
 export default function NavSide() {
-    return (
-        <>
-        <div className="h-full w-full flex flex-col p-2">
-            <div className="header">hi</div>
-            <div>Items</div>
-        </div>
-        </>
-    )
+	return (
+		<>
+			<div className='h-full w-full flex flex-col p-2'>
+				<div className='header flex-row'>
+					<div>
+						<span>Reader</span>
+					</div>
+					<div>
+						<div className='h-px w-2 bg-black'></div>
+						<div className='h-px w-2 bg-black'></div>
+					</div>
+				</div>
+				<div>Items</div>
+			</div>
+		</>
+	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import ListSide from "./components/listSide/listSide";
-import NavSide from "./components/navSide/navSide";
-import Content from "./components/content/content";
+'use client'
+
+import ListSide from './components/listSide/listSide'
+import NavSide from './components/navSide/navSide'
+import Content from './components/content/content'
 
 export default function Home() {
-  return (
-    <div className="container mx-auto p-4 w-full h-screen flex flex-row">
-      <div className="w-1/4 hidden md:block border-r border-border "><NavSide/></div>
-      <div className="w-full md:w-1/4 border-r border-border "><ListSide/></div>
-      <div className="w-1/2 hidden md:block "><Content/></div>
-    </div>
-  )
+	return (
+		<div className='container mx-auto p-4 w-full h-screen flex flex-row'>
+			<div className='w-2/10 hidden lg:block border-r border-border '>
+				<NavSide />
+			</div>
+			<div className='w-full md:w-3/10 border-r border-border '>
+				<ListSide />
+			</div>
+			<div className='w-1/2 hidden md:block '>
+				<Content />
+			</div>
+		</div>
+	)
 }
